e51837b80515b71be6aa4817e6f9738b2c7ab261
Kunjannn26 <87530425+Kunjannn26@users.noreply.github.com>
2021-09-21 22:37:06 +0530

    commit on 'main'
    Signed-off-by: Kunjannn26 <87530425+Kunjannn26@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//I've tried to explain each JavaScript line with comments....Hope you'll understand
+
 
 //selecting all required elements
 const selectBox = document.querySelector(".select-box"),
@@ -11,27 +11,26 @@ resultBox = document.querySelector(".result-box"),
 wonText = resultBox.querySelector(".won-text"),
 replayBtn = resultBox.querySelector("button");
 
-window.onload = ()=>{ //once window loaded
-    for (let i = 0; i < allBox.length; i++) { //add onclick attribute in all available span
+window.onload = ()=>{ 
+    for (let i = 0; i < allBox.length; i++) { 
        allBox[i].setAttribute("onclick", "clickedBox(this)");
     }
 }
 
 selectBtnX.onclick = ()=>{
-    selectBox.classList.add("hide"); //hide select box
-    playBoard.classList.add("show"); //show the playboard section
+    selectBox.classList.add("hide"); 
+    playBoard.classList.add("show");
 }
 
 selectBtnO.onclick = ()=>{ 
-    selectBox.classList.add("hide"); //hide select box
-    playBoard.classList.add("show"); //show the playboard section
-    players.setAttribute("class", "players active player"); //set class attribute in players with players active player values
-}
+    selectBox.classList.add("hide"); 
+       playBoard.classList.add("show"); 
+    players.setAttribute("class", "players active player"); }
 
-let playerXIcon = "fas fa-times"; //class name of fontawesome cross icon
-let playerOIcon = "far fa-circle"; //class name of fontawesome circle icon
-let playerSign = "X"; //this is a global variable beacuse we've used this variable inside multiple functions
-let runBot = true; //this also a global variable with boolen value..we used this variable to stop the bot once match won by someone or drawn
+let playerXIcon = "fas fa-times"; 
+let playerOIcon = "far fa-circle"; 
+let playerSign = "X";
+let runBot = true; 
 
 // user click function
 function clickedBox(element){
@@ -45,12 +44,12 @@ function clickedBox(element){
         element.setAttribute("id", playerSign); //set id attribute in span/box with player choosen sign
         players.classList.add("active"); ///add active class in players
     }
-    selectWinner(); //calling selectWinner function
+    selectWinner();
     element.style.pointerEvents = "none"; //once user select any box then that box can'be clicked again
     playBoard.style.pointerEvents = "none"; //add pointerEvents none to playboard so user can't immediately click on any other box until bot select
     let randomTimeDelay = ((Math.random() * 1000) + 200).toFixed(); //generating random number so bot will randomly delay to select unselected box
     setTimeout(()=>{
-        bot(runBot); //calling bot function
+        bot(runBot); 
     }, randomTimeDelay); //passing random delay value
 }
 
@@ -76,7 +75,7 @@ function bot(){
                 players.classList.remove("active"); //remove active class in players
                 allBox[randomBox].setAttribute("id", playerSign); //set id attribute in span/box with player choosen sign
             }
-            selectWinner(); //calling selectWinner function
+            selectWinner(); 
         }
         allBox[randomBox].style.pointerEvents = "none"; //once bot select any box then user can't click on that box
         playBoard.style.pointerEvents = "auto"; //add pointerEvents auto in playboard so user can again click on box
@@ -95,25 +94,25 @@ function checkIdSign(val1, val2, val3, sign){ //checking all id value is equal t
 function selectWinner(){ //if the one of following winning combination match then select the winner
     if(checkIdSign(1,2,3,playerSign) || checkIdSign(4,5,6, playerSign) || checkIdSign(7,8,9, playerSign) || checkIdSign(1,4,7, playerSign) || checkIdSign(2,5,8, playerSign) || checkIdSign(3,6,9, playerSign) || checkIdSign(1,5,9, playerSign) || checkIdSign(3,5,7, playerSign)){
         runBot = false; //passing the false boolen value to runBot so bot won't run again
-        bot(runBot); //calling bot function
-        setTimeout(()=>{ //after match won by someone then hide the playboard and show the result box after 700ms
+        bot(runBot);
+        setTimeout(()=>{ 
             resultBox.classList.add("show");
             playBoard.classList.remove("show");
-        }, 700); //1s = 1000ms
+        }, 700); 
         wonText.innerHTML = `Player <p>${playerSign}</p> won the game!`; //displaying winning text with passing playerSign (X or O)
     }else{ //if all boxes/element have id value and still no one win then draw the match
         if(getIdVal(1) != "" && getIdVal(2) != "" && getIdVal(3) != "" && getIdVal(4) != "" && getIdVal(5) != "" && getIdVal(6) != "" && getIdVal(7) != "" && getIdVal(8) != "" && getIdVal(9) != ""){
-            runBot = false; //passing the false boolen value to runBot so bot won't run again
-            bot(runBot); //calling bot function
-            setTimeout(()=>{ //after match drawn then hide the playboard and show the result box after 700ms
+            runBot = false;
+            bot(runBot); 
+            setTimeout(()=>{ 
                 resultBox.classList.add("show");
                 playBoard.classList.remove("show");
-            }, 700); //1s = 1000ms
-            wonText.textContent = "Match has been drawn!"; //displaying draw match text
+            }, 700);
+            wonText.textContent = "Match has been drawn!"; 
         }
     }
 }
 
 replayBtn.onclick = ()=>{
-    window.location.reload(); //reload the current page on replay button click
+    window.location.reload(); 
 }
